cbe09557963e910db17c35953f667fbbeaf41602
kalina <f08907@student.nbu.bg>
2023-10-18 07:37:57 +0300

    BoardR

---
+++ BoardR/src/BoardItem.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+
+enum Status {
+    Open, Todo, InProgress, Done, Verified
+}
+
+public class BoardItem {
+    public String title;
+    public LocalDate dueDate;
+    public Status status;
+
+    public BoardItem(String title, LocalDate dueDate){
+        if (title == null || title.trim().isEmpty()){
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        if(title.length() < 5 || title.length() > 30){
+            throw new IllegalArgumentException("Title length should be between 5 and 30 characters");
+        }
+        if(dueDate.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("Due date connot be in the past");
+        }
+        this.title = title;
+        this.dueDate = dueDate;
+        this.status = Status.Open;
+    }
+
+}
