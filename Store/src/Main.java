3bf1902c0246574891974a05336801ef8d56c6a8
kalina <borissova.k@icloud.com>
2023-10-18 10:58:59 +0300

    Store

--- Store/src/Main.java
+++ Store/src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Order order1 = new Order("John", 400, "BGN",
+                LocalDate.now(), new String[]{"PC", "Keyboard", "Mouse"});
+
+        Order order2 = new Order("Frank", 120, "EUR",
+                LocalDate.now(), new String[]{"Jeans", "T-shirt"});
+
+        Order[] orders = new Order[]{order1, order2};
+
+        for(Order order : orders){
+            order.displayGeneralInfo();
+            System.out.println();
+            order.displayOrderItems();
+            System.out.println("------------------");
+        }
     }
 }
--- Store/src/Order.java
+++ Store/src/Order.java
@@ -16,4 +16,20 @@ public class Order {
         this.deliveryOn = deliveryOn;
         this.items = items;
     }
+
+    public void displayOrderItems(){
+        if(items.length == 0){
+            System.out.println("No items!");
+        }
+        StringBuilder builder = new StringBuilder("Items: ");
+        for(String item : items){
+            builder.append(String.format("%s ", item));
+        }
+        System.out.println(builder.toString());
+    }
+
+    public void displayGeneralInfo(){
+        System.out.printf("Order to: %s | Delivery on: %s | Total: %s",
+                            recipient, deliveryOn, total);
+    }
 }
